083880effae27ed00c050dacd634cd454ddd7af6
crispi <cristina.picatoste@gmail.com>
2023-11-17 21:53:35 +0100

    Planteamiento ejercicio todo list front

--- src/pages/MyForm.tsx
+++ src/pages/MyForm.tsx
@@ -1,50 +1,69 @@
 /* eslint-disable */
 
-import React from 'react';
-import { useForm } from 'react-hook-form';
-import DynamicInput from '../components/DynamicInput';
-import { ErrorForm } from '../components/ErrorForm';
+import React, { useState } from 'react';
 
 const MyForm = () => {
-    const { register, handleSubmit, formState: { errors } } = useForm();
+    const [todo, setTodo] = useState({
+        title: '', // input type text
+        description: '', // input type textarea
+        status: '', // input type text
+        datestart: '', // input type date
+        dateend: '', // input type date
+        userId: '2314rtyu8923irj4ntfh12', // usuario fake // input type text
+        //id --> logica gestionada en backend
+        //createdAt --> logica gestionada en backend
+        //modifiedAt --> logica gestionada en backend
+        //deletedAt --> logica gestionada en backend
+    });
+    const [todoList, setTodoList] = useState([]);
+    //const { register, handleSubmit, formState: { errors } } = useForm();
 
-    const onSubmit = data => {
-        console.log(data);
-    };
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+        setTodo({
+            ...todo,
+            [name]: value
+        })
+    } 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // fetch('url del enpoint, method POST', todo)
+        //     .then(res => res.json())
+        //     .then(data => setTodoList([...todoList, data]))
+        //     .catch
 
+    }
+    
+    const inputStyles = "text-sm bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500";
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <DynamicInput
-                type="text"
-                name="username"
-                label="Nombre de usuario"
-                placeholder="Username"
-                {...register('username', { required: 'Username is required' })}
-            />
-            {errors.username && <ErrorForm error={errors.username.message} />}
+        <>
+            <form onSubmit={handleSubmit}>
 
-            <DynamicInput
-                type="email"
-                name="email"
-                label="Correo electrónico"
-                placeholder="Email"
-                {...register('email', { required: 'Email is required' })}
-            />
-            {errors.email && <ErrorForm error={errors.email.message} />}
+                <label className='text-sm pb-1 text-left text-gray-700'>Add your todo</label>
+                <input 
+                    type="text"
+                    name="todo"
+                    placeholder="Username"
+                    onChange={handleChange}
+                    className={inputStyles}
+                />
 
-            <DynamicInput
-                type="password"
-                name="credentials"
-                label="Credenciales"
-                placeholder="Credentials"
-                {...register('credentials', { required: 'Credentials is required' })}
-            />
-            {errors.credentials && <ErrorForm error={errors.credentials.message} />}
+                <button type="submit">Submit</button>
+            </form>
+            {todoList}
+        </>
 
-
-            <button type="submit">Submit</button>
-        </form>
     );
 };
 
 export default MyForm;
+
+{/* <DynamicInput
+type="password"
+name="credentials"
+label="Credenciales"
+placeholder="Credentials"
+{...register('credentials', { required: 'Credentials is required' })}
+/>
+{errors.credentials && <ErrorForm error={errors.credentials.message} />} */}
+
